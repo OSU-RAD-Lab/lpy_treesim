da9c2588fbd995cdc4586d8fd7620709d60c9a7d
cindygr <cindy.grimm@oregonstate.edu>
2026-07-13 10:32:42 -0700

    Fixing ufo tying

--- lpy_treesim/tie_prune/tie_prune_simulation_base.py
+++ lpy_treesim/tie_prune/tie_prune_simulation_base.py
@@ -271,7 +271,7 @@ class TreeSimulationBase(ABC):
                 continue
 
             if branch.tying.tie_type == TyingState.TyingType.TIE_ACROSS:
-                check_coord = 1  # UFO-style - check that x index is within 2/3 of tie spacing
+                check_coord = 0  # UFO-style - check that x index is within 2/3 of tie spacing
                 spacing = self.support.spacing_across_wire()
             else:
                 check_coord = 2  # Envy-style - check that z index is within 2/3 of wire spacing

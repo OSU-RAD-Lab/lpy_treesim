3d3850da26d23c05f3032cfb39d4a5a5b540bedd
OSU-RAD-Lab-Admin <osuradlab@gmail.com>
2026-07-14 21:31:30 +0000

    Primarily entering KNN Algo. Using KDTree from scipy

--- lpy_treesim/tie_prune/pruning_algo/prune_at_end.py
+++ lpy_treesim/tie_prune/pruning_algo/prune_at_end.py
@@ -4,13 +4,16 @@ from math import sqrt
 def get_dist(L1, L2):
     return sqrt((L1.x-L2.x)**2+(L1.y-L2.y)**2+(L1.z-L2.z)**2)
 
-def end_prune(self, branch_hierarchy: dict, map_names_to_branches: dict):
+# changed from (self, branch_hierarchy: dict, map_names_to_branches: dict):
+def end_prune(tree_sim_base, branch_hierarchy: dict, map_names_to_branches: dict):
     bud_list = []
     names_to_x = []
     prune_locations = []
+    
     for name, branch in map_names_to_branches.items():
         if "bud" in name:
             bud_list.append(branch)
+            prune_locations.append(branch.start_loc)
     
     referance = None
     for referance_bud, comparison_bud in combo(bud_list,2):
@@ -18,13 +21,18 @@ def end_prune(self, branch_hierarchy: dict, map_names_to_branches: dict):
             within_list = []
             referance = referance_bud
         dist = get_dist(referance_bud.start_loc, comparison_bud.start_loc)
+        
         if dist < 0.1:
-            #names_to_x.extend(comparison_bud.prune())
-            prune_locations.append(referance_bud.start_loc)
+        
+            names_to_x.extend(comparison_bud.prune())
+          #  prune_locations.append(referance_bud.start_loc)
             #print(f"[DEBUG] {comparison_branch.name} becuase of {referance_branch.name} at {dist}")
     
-    map_names_to_branches['marked_locations'] = prune_locations
+
 
     for name in names_to_x:
         del branch_hierarchy[name]
         del map_names_to_branches[name]
+        
+    # Hand the locations back to prune_tree so the 3D plot can show what was cut
+    return prune_locations
